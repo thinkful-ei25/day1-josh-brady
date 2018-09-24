e9cb4220e5c72ed9cadd9052c1341cfe165a2ce1
Box <bradycfox@gmail.com>
2018-09-24 15:36:06 -0400

    craking the code

--- set2.js
+++ set2.js
@@ -1,9 +1,10 @@
 'use strict';
 
+//Jedi Name
 function jediName (firstName, lastName){
   return lastName.slice(0,3).concat(firstName.slice(0,2));
 }
-
+// To infinity...
 function beyond(num){
   if(!Number.isFinite(num)){
     console.log('And beyond');
@@ -21,5 +22,35 @@ function beyond(num){
 
 }
 
+//Cracking
+
+function decode(encodedStr) {
+  let newArray = encodedStr.split(' ');
+  let returnStr = "";
+  for (let i = 0; i < newArray.length; i++) {
+    switch(newArray[i][0]) {
+        case "a":
+            returnStr += newArray[i][1];
+            break;
+        case "b":
+            returnStr += newArray[i][2];
+            break;
+        case "c":
+            returnStr += newArray[i][3];
+            break;
+        case "d":
+            returnStr += newArray[i][4];
+            break;
+        default:
+            returnStr+= ' ';
+            break;
+    }
+  }
+  return returnStr;
+}
+
+
+//Tests
 console.log(jediName('brady', 'douglas'));
 beyond(Infinity);
+console.log(decode('craft block argon meter bells brown croon droop'));
