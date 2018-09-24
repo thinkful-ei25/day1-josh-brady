6b9a1610a2ba2006885974bcad3783d472aaad3a
Joshua Adams <joshadams554@gmail.com>
2018-09-24 14:07:49 -0400

    question3 finished

--- set1.js
+++ set1.js
@@ -1,7 +1,8 @@
 'use strict';
 function createGreeting() {
-  console.log('Hi, my name is Josh and I\'m 29 years old');
+  return 'Hi, my name is Josh and I\'m 29 years old';
 }
 
-createGreeting();
+let greeting = createGreeting();
 
+console.log(greeting);
