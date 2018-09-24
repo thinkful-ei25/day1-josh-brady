214267c0841d8282be3287cefe3142b22d7ee5b9
Joshua Adams <joshadams554@gmail.com>
2018-09-24 14:14:57 -0400

    finsihed question 5

--- set1.js
+++ set1.js
@@ -1,6 +1,7 @@
 'use strict';
 function createGreeting(name, age) {
-  return `Hi, my name is ${name} and I'm ${age} years old.`;
+  const yearOfBirth = 2018 - age;  
+  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
 let greeting = createGreeting('brady', 29);
