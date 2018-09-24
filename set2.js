9fb471bb3c25a796369e39e8a5f692fdbe6b93e2
Box <bradycfox@gmail.com>
2018-09-24 16:10:46 -0400

    rock paper scissors

--- set2.js
+++ set2.js
@@ -22,28 +22,28 @@ function beyond(num){
 
 }
 
-//Cracking
+//Cracking the code
 
 function decode(encodedStr) {
   let newArray = encodedStr.split(' ');
   let returnStr = "";
   for (let i = 0; i < newArray.length; i++) {
     switch(newArray[i][0]) {
-        case "a":
-            returnStr += newArray[i][1];
-            break;
-        case "b":
-            returnStr += newArray[i][2];
-            break;
-        case "c":
-            returnStr += newArray[i][3];
-            break;
-        case "d":
-            returnStr += newArray[i][4];
-            break;
-        default:
-            returnStr+= ' ';
-            break;
+    case "a":
+      returnStr += newArray[i][1];
+      break;
+    case "b":
+      returnStr += newArray[i][2];
+      break;
+    case "c":
+      returnStr += newArray[i][3];
+      break;
+    case "d":
+      returnStr += newArray[i][4];
+      break;
+    default:
+      returnStr+= ' ';
+      break;
     }
   }
   return returnStr;
@@ -56,40 +56,61 @@ function daysInMonth(month, leap = false){
     return 'february has 29 days';
   }
   switch(month){
-    case 'february':
-      days = 28;
-      break;
-    case 'september':
-    case 'april':
-    case 'june':
-    case 'november':
-      days = 30;
-      break;
-    case 'january':
-    case 'march':
-    case 'may':
-    case 'july':
-    case 'august':
-    case 'october':
-    case 'december':
-      days = 31;
-      break;
-    default:
-      throw new Error('must provide a valid month');
+  case 'february':
+    days = 28;
+    break;
+  case 'september':
+  case 'april':
+  case 'june':
+  case 'november':
+    days = 30;
+    break;
+  case 'january':
+  case 'march':
+  case 'may':
+  case 'july':
+  case 'august':
+  case 'october':
+  case 'december':
+    days = 31;
+    break;
+  default:
+    throw new Error('must provide a valid month');
   }
   return `${month} has ${days} days`;
-
-
-
 }
 
+//Rock Paper Scissors
+
+function rockPaperScissors (num) {
+  if (num < 1 || num > 3) {
+    throw new Error ('please enter number 1, 2 or 3');
+  } 
+  const randomNo = Math.floor(Math.random() * 3) +1;
+  let result = num - randomNo;
+
+  if (result === 1 || result === -2) {
+    return 'You are the winner!';
+  } else if (result === 0) {
+    return 'It was a tie!';
+  } else {
+    return 'You lost :(';
+  }
+
+}
 
 //Tests
 console.log(jediName('brady', 'douglas'));
 beyond(Infinity);
 console.log(decode('craft block argon meter bells brown croon droop'));
+
 try{
   console.log(daysInMonth('February', true));
 } catch(e) {
+  console.error(e.message);
+}
+try {
+  console.log(rockPaperScissors(3));
+} catch (e) {
   console.error(e.message);
 }
