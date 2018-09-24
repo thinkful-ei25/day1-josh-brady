5fdf236194607002582ef6dfadba86741091026b
Box <bradycfox@gmail.com>
2018-09-24 14:20:48 -0400

    created getYearOfBirth Function

--- set1.js
+++ set1.js
@@ -1,7 +1,11 @@
 'use strict';
 function createGreeting(name, age) {
-  const yearOfBirth = 2018 - age;  
+  const yearOfBirth = getYearOfBirth(age);  
   return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
+}
+
+function getYearOfBirth(age) {
+  return 2018-age;
 }
 
 let greeting = createGreeting('brady', 29);
