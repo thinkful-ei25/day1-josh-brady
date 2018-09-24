6de770e5e6e74079357caf7368f572115aea49a0
Joshua Adams <joshadams554@gmail.com>
2018-09-24 15:21:33 -0400

    added function beyond

--- set2.js
+++ set2.js
@@ -4,4 +4,22 @@ function jediName (firstName, lastName){
   return lastName.slice(0,3).concat(firstName.slice(0,2));
 }
 
+function beyond(num){
+  if(!Number.isFinite(num)){
+    console.log('And beyond');
+    return;
+  }
+  if(num > 0){
+    console.log('To infinity');
+    return;
+  }
+  if(num < 0){
+    console.log('To negative infinity');
+    return;
+  }
+  console.log('Staying home');
+
+}
+
 console.log(jediName('brady', 'douglas'));
+beyond(Infinity);
